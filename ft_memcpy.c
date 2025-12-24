779c9b861c6ac91fb836e99c0cfd7469ca4fcf3a
Majd Bdour <126000499+majd-joy@users.noreply.github.com>
2025-12-24 23:03:28 +0300

    The succes att.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,22 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: majbdour <majbdour@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/26 16:19:18 by majbdour          #+#    #+#             */
-/*   Updated: 2025/12/24 20:07:45 by majbdour         ###   ########.fr       */
+/*   Created: 2025/12/24 20:30:23 by majbdour          #+#    #+#             */
+/*   Updated: 2025/12/24 20:30:29 by majbdour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char		*d;
-	const unsigned char	*s;
+	unsigned char			*d;
+	const unsigned char		*s;
+	size_t					i;
 
-	if (!dst || !src)
+	if (!dest && !src)
 		return (NULL);
-	d = (unsigned char *)dst;
+	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-	while (n--)
-		*d++ = *s++;
-	return (dst);
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,34 +6,30 @@
 /*   By: majbdour <majbdour@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 20:59:46 by majbdour          #+#    #+#             */
-/*   Updated: 2025/12/24 20:08:02 by majbdour         ###   ########.fr       */
+/*   Updated: 2025/12/24 20:31:41 by majbdour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
-	size_t			i;
+	unsigned char		*d;
+	const unsigned char	*s;
 
-	if (!dst || !src)
+	if (!dest && !src)
 		return (NULL);
-	d = (unsigned char *)dst;
-	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
 	if (d > s)
 	{
-		while (len--)
-			d[len] = s[len];
-	}
-	else
-	{
-		i = 0;
-		while (i < len)
+		while (n > 0)
 		{
-			d[i] = s[i];
-			i++;
+			n--;
+			d[n] = s[n];
 		}
 	}
-	return (dst);
+	else
+		ft_memcpy(dest, src, n);
+	return (dest);
 }
